66ea15bebbd9b4d819107abb52229bbda68d1957
thanhquisg24 <thanhquisg24@gmail.com>
2023-02-01 13:56:42 +0700

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
-import entities from './typeorm';
+// import entities from './config/typeorm.entities';
 import { ConfigModule } from '@nestjs/config';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: ['.env'],
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'soldier001',
-      database: 'nestjs',
-      entities,
-      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      dropSchema: false,
-    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'soldier001',
+    //   database: 'nestjs',
+    //   entities,
+    //   // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    //   dropSchema: false,
+    // }),
     // TaskModule,
     ArticleModule,
   ],
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,58 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import entities from './typeorm.entities';
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return {
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      entities,
+      migrationsTableName: 'migrations',
+      migrations: [
+        'src/database/migrations/*.ts',
+        // path.resolve(`${__dirname}/../../database/migrations/*{.ts,.js}`),
+      ],
+
+      //   cli: {
+      //     migrationsDir: __dirname + '/../database/migrations',
+      //   },
+      extra: {
+        charset: 'utf8mb4_unicode_ci',
+      },
+      synchronize: false,
+      logging: true,
+      logger: 'file',
+    };
+  },
+};
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  entities,
+  migrationsTableName: 'migrations',
+  migrations: [`src/database/migrations/*{.ts,.js}`],
+  //   cli: {
+  //     migrationsDir: __dirname + '/../database/migrations',
+  //   },
+  extra: {
+    charset: 'utf8mb4_unicode_ci',
+  },
+  synchronize: false,
+  logging: true,
+  logger: 'file',
+};
